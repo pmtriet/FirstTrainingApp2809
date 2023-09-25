2db0f258832e46488602ab4ca3e9e43115164dac
Fy Spoti <>
2023-09-25 16:53:23 +0700

    chinh lai cach kiem tra dieu kien

--- firstlesson/ViewController.swift
+++ firstlesson/ViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    var isImaganhdep : Bool = false
     @IBOutlet weak var imageView: UIImageView!
     
     
@@ -18,19 +18,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
-
-    @IBAction func changeClick(_ sender: Any) {
-        if imageView.image == UIImage(named: "anh")//tham khao cach kiem tra ten anh de tao ra cau lenh dieu khien if else
+    
+    @IBAction func changeClick(_ sender: Any)
+    {
+        
+        if !isImaganhdep//tham khao cach kiem tra ten anh de tao ra cau lenh dieu khien if else
         {
-        imageView.image = UIImage(named: "anhdep")
-            
+            imageView.image = UIImage(named: "anhdep")
+            isImaganhdep = true
         } else {
-            imageView.image = UIImage(named: "anh")
+            imageView.image = UIImage(named: "anh1")
+            isImaganhdep = false
         }
+        
     }
     
-    
-    
-
 }
+
 
