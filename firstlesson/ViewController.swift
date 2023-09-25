9a16d83de47fa2bccf1d0d75abdcdd149593e7e7
Fy Spoti <>
2023-09-25 15:50:07 +0700

    25/09

--- firstlesson/ViewController.swift
+++ firstlesson/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func changeClick(_ sender: Any) {
+        imageView.image = UIImage(named: "anhdep")
+            
+    }
+    
+    
+    
 
 }
 
