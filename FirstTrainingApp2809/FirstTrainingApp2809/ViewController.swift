26e7a8efa26a0f647d94abf56e08f92b82a9df6a
Minh Triet Pham <>
2023-09-28 21:22:22 +0700

    Change image with code

--- FirstTrainingApp2809/FirstTrainingApp2809/ViewController.swift
+++ FirstTrainingApp2809/FirstTrainingApp2809/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickedChange(_ sender: Any) {
+//        if imageView.image == UIImage(named: "square.and.arrow.up") {
+//            imageView.image = UIImage(named: "rectangle.portrait.and.arrow.right")
+//        } else {
+//            imageView.image = UIImage(named: "square.and.arrow.up")
+//        }
+        if imageView.image == UIImage(systemName: "square.and.arrow.up") {
+            imageView.image = UIImage(systemName: "rectangle.portrait.and.arrow.right")
+        } else {
+            imageView.image = UIImage(systemName: "square.and.arrow.up")
+        }
+    }
+    
 }
 
