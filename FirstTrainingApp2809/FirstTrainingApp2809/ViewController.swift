4ade9cc62482b69327a90553e9cc55d069d617d8
Minh Triet Pham <>
2023-09-28 16:45:10 +0700

    initial commit

---
+++ FirstTrainingApp2809/FirstTrainingApp2809/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstTrainingApp2809
+//
+//  Created by Minh Triet Pham on 28/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
